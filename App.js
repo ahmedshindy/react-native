840a22074bb842fed3278703c7f949c0e7a33951
ahmedshindy <ahmedshindy70@gmail.com>
2024-04-18 16:37:53 +0200

    commit ^-^

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName ] = useState('Ahmed');
+  const [age,setAge] = useState('30');
+  const clickHandler = () =>{
+    setName('Ahmed Hammad Shindy');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText} > Hello Shindy!</Text>
+        <Text>Lorem ipsum <Text style={styles.boldText}>dolor</Text> sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
+
+      <View>
+        <Text style= {styles.boldText}>{name}</Text>
+        <Button title='print full name' onPress={clickHandler }  />
+
+      </View>
+      <Text>Enter name:</Text>
+      <TextInput 
+        placeholder='e.g. 99' 
+        style={styles.input}
+        onChangeText={(value) => setAge(value)} />
+
+      <Text>Enter age:</Text>
+      <TextInput 
+        placeholder='e.g. John Doe' 
+        style={styles.input}
+        onChangeText={(value) => setName(value)} />
+      <View>
+
+      </View>
     </View>
   );
 }
@@ -17,4 +47,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding:20,
+  },
+  boldText: {
+     fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  input: {
+    borderWidth:1,
+    borderColor:'#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  }
 });
+
+
+// npm start
